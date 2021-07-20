0d52c020e124d3b2821310235523ecd7928b86dc
Gustavo-F <fgustavo016@gmail.com>
2021-07-19 23:11:27 -0300

    Adicionado beforeEach e assumeTrue para verificação de páginas

--- kindle/src/test/java/kindle/BookTest.java
+++ kindle/src/test/java/kindle/BookTest.java
@@ -1,17 +1,19 @@
 package kindle;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assume.assumeFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-import kindle.db.AuthorDAO;
-import kindle.db.PublisherDAO;
 import kindle.db.UtilDB;
-import kindle.entities.Author;
+import kindle.db.AuthorDAO;
 import kindle.entities.Book;
+import kindle.db.PublisherDAO;
+import kindle.entities.Author;
 import kindle.entities.Publisher;
 
 class BookTest {
@@ -30,15 +32,19 @@ class BookTest {
 		book = new Book("Livro", 150, publisher);
 	}	
 	
+	@BeforeEach
+	void checkBook() {
+		assertNotNull(book);
+	}
+		
 	@Test
 	void testAuthorsList() {
 		assumeFalse(book.verifyAuthorsList());
-		assumeFalse(book.verifyPages());
 	}
 	
 	@Test
 	void testPages(){
-		
+		assumeTrue(book.verifyPages());
 	}
 	
 	@AfterAll
