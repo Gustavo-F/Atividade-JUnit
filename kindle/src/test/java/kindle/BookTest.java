96af2ae4e8d347561d8880d52b99f8f11f2896e2
Gustavo-F <fgustavo016@gmail.com>
2021-07-19 23:55:24 -0300

    Adicionado assertNull e persistencia do livro em afterAll

--- kindle/src/test/java/kindle/BookTest.java
+++ kindle/src/test/java/kindle/BookTest.java
@@ -1,40 +1,51 @@
 package kindle;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assume.assumeFalse;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-import org.junit.jupiter.api.Test;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
-import kindle.db.UtilDB;
 import kindle.db.AuthorDAO;
-import kindle.entities.Book;
+import kindle.db.BookDAO;
 import kindle.db.PublisherDAO;
+import kindle.db.UtilDB;
 import kindle.entities.Author;
+import kindle.entities.Book;
 import kindle.entities.Publisher;
 
 class BookTest {
 	
 	static Book book = null;
+	static Publisher publisher;
 
+	@Timeout(value = 5, unit = TimeUnit.SECONDS)
 	@BeforeAll
 	static void init() {
-		System.out.println("@SuppressWarnings(\"unused\")");
 		Author author = new Author("Gustao Ferreira Souza");
 		author.setId(AuthorDAO.persist(author));
 		
 		Publisher publisher = new Publisher("Veja");
 		publisher.setId(PublisherDAO.persist(publisher));
 
-		book = new Book("Livro", 150, publisher);
+		book = new Book("Livro", 150, null);
 	}	
 	
 	@BeforeEach
 	void checkBook() {
 		assertNotNull(book);
+	}
+	
+	@Test
+	void checkPublisher() {
+		assertNull(book.getPublisher());
 	}
 		
 	@Test
@@ -49,6 +60,7 @@ class BookTest {
 	
 	@AfterAll
 	static void close() {
+		BookDAO.persist(book);
 		UtilDB.closeConn();
 	}
 
